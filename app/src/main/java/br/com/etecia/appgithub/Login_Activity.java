3188e516bb954a0cef6068e4a96edab37e1a39ec
LAB <joaomunduruca@outlook.com>
2018-03-20 20:27:05 -0300

    aula 20/03

--- app/src/main/java/br/com/etecia/appgithub/Login_Activity.java
+++ app/src/main/java/br/com/etecia/appgithub/Login_Activity.java
@@ -2,6 +2,9 @@ package br.com.etecia.appgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import javax.swing.text.View;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+    }
+    public void entrarSistema(View view){
+        Toast.makeText(Login_Activity.this,"entrarSistema em contrução",Toast.LENGTH_SHORT).show();
+
+    }
+    public void sairSistema(View view){
+        Toast.makeText(Login_Activity.this,"sairSistema em contrução",Toast.LENGTH_SHORT).show();
     }
 }
