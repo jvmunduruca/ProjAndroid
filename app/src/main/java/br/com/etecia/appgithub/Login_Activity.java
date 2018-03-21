76bb782c6627dde8ff0344b1340fc764958ff2d5
LAB <joaomunduruca@outlook.com>
2018-03-20 22:02:29 -0300

    aula 20/03 - 2 - Nova Activity

--- app/src/main/java/br/com/etecia/appgithub/Login_Activity.java
+++ app/src/main/java/br/com/etecia/appgithub/Login_Activity.java
@@ -3,21 +3,39 @@ package br.com.etecia.appgithub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login_Activity extends AppCompatActivity {
+
+    EditText txtUsuario;
+    EditText txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+        txtUsuario = (EditText) findViewById(R.id.txtUsuario);
+        txtSenha = (EditText) findViewById(R.id.txtSenha);
     }
 
     public void sairSistema(View view){
-        Toast.makeText(Login_Activity.this,"sairSistema em contrução",Toast.LENGTH_SHORT).show();
+        String usuario = txtUsuario.getText().toString();
+        String senha = txtSenha.getText().toString();
+        usuario = "";
+        senha = "";
+        Toast.makeText(Login_Activity.this,txtUsuario.getText()+"sairSistema em contrução",Toast.LENGTH_SHORT).show();
     }
 
     public void entrarSistema(View view) {
-        Toast.makeText(Login_Activity.this,"entrarSistema em contrução",Toast.LENGTH_SHORT).show();
+        String usuario = txtUsuario.getText().toString();
+        String senha = txtSenha.getText().toString();
+
+        if(usuario.equals("etecia") && senha.equals("etecia")){
+            Toast.makeText(Login_Activity.this,txtUsuario.getText()+" Bem vindo ao Sistema",Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(Login_Activity.this,"Senha  ou Usuário Invalidos",Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
