a750c613cd37cbe5fd0b4213232068a8cf9b453d
LAB <joaomunduruca@outlook.com>
2018-03-20 21:12:23 -0300

    aula 20/03 - corrigindo botões

--- app/src/main/java/br/com/etecia/appgithub/Login_Activity.java
+++ app/src/main/java/br/com/etecia/appgithub/Login_Activity.java
@@ -2,9 +2,8 @@ package br.com.etecia.appgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
-
-import javax.swing.text.View;
 
 public class Login_Activity extends AppCompatActivity {
 
@@ -13,11 +12,12 @@ public class Login_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
     }
-    public void entrarSistema(View view){
-        Toast.makeText(Login_Activity.this,"entrarSistema em contrução",Toast.LENGTH_SHORT).show();
 
-    }
     public void sairSistema(View view){
         Toast.makeText(Login_Activity.this,"sairSistema em contrução",Toast.LENGTH_SHORT).show();
+    }
+
+    public void entrarSistema(View view) {
+        Toast.makeText(Login_Activity.this,"entrarSistema em contrução",Toast.LENGTH_SHORT).show();
     }
 }
